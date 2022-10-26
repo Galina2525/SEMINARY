3146fd002438d24c091a957d9a2ae382d7ee5f7b
Galina <koroleva2525.gk@gmail.com>
2022-10-26 10:00:48 +0200

    fix task0, gitignore

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -5,7 +5,8 @@
 // -7 ->49
 
 Console.Write("Enter number"); //1. Приглашение к вводу
-//int number = Convert.ToInt32(Console.ReadLine()); // 2.Считывание данных с консоли и преобразование в тип int
+//int number = Convert.ToInt32(Console.ReadLine()); // 2.Считывание данных с консоли и преобразование
+// в тип int
 string? str = Console.ReadLine();// Если подробнее то 2.1 Считываем данные с консоли
 int number = Convert.ToInt32(str);// 2.2 Конвертация в число
 Console.Write(number*number); // Вывод на экран number  в квадрате
