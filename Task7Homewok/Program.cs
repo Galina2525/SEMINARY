5b22ba092c95b602c1ef9c5bd9a80147b016d55f
Galina <koroleva2525.gk@gmail.com>
2022-10-27 20:56:28 +0200

    add task6 and7

---
+++ Task7Homewok/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным 
+//(делится ли без остатка на два)
+// 4 -> да
+// -3 -> нет
+
+Console.WriteLine("Enter number ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 ==0){
+    Console.WriteLine("Yes");
+}
+else{
+    Console.WriteLine("No");
+}
+
+
