9747083ae76ea15d1cef3defd3a57dfa09fb483e
Galina <koroleva2525.gk@gmail.com>
2022-10-27 14:46:35 +0200

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнее
+// 456->6
+// 782->2
+
+Console.WriteLine("Enter number  ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number % 10);
